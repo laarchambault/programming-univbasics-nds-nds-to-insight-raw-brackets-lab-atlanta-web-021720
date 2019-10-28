65df08ba59b371653b56849059da5d8f16daa1ec
Laura Archambault <laarchambault@gmail.com>
2019-10-28 23:50:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,10 +15,10 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
   require 'pp'
-  
+  #director_name = directors_database[row_index][:name]
 row_index = 0
 while row_index < directors_database.length do
-  puts directors_database[row_index][:name]
+  puts directors_database[row_index]
   row_index += 1
 end
 end
